6286e7803c4234cc4ecd774471ff1a2f8b938243
i20hirota <i20tokuyama.kosen-ac.jp>
2025-01-27 09:53:03 +0900

    抽象クラスをインターフェースに変更して，動作を確認しました

---
+++ src/ex09/GrayImage.java
@@ -0,0 +1,28 @@
+package ex09;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class GrayImage extends BufferedImage {
+  public GrayImage(BufferedImage img) {
+    super(img.getWidth(), img.getHeight(), TYPE_BYTE_GRAY);
+    Graphics g = createGraphics();
+    g.drawImage(img, 0, 0, null);
+  }
+  
+  public int getGray(int x, int y) {
+    int argb = getRGB(x, y);
+    int l = argb & 0xff;
+    
+    return l;
+  }
+  
+  public void setGray(int x, int y, int gray) {
+    int argb = (0xff<<24) | (gray<<16) | (gray<<8) | gray;
+    setRGB(x, y, argb);
+  }
+  
+  public void applyFilter(Processable f) {
+    f.process(this);
+  }
+}
